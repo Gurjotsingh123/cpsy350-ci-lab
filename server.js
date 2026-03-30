c251f460b9c568431b4b63522673161681962b1d
Gurjot Singh <yourgithubemail@gmail.com>
2026-03-30 15:46:13 -0400

    000970543

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:000970543');
+  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:00097054 gurjot3');
 });
 
 const server = app.listen(PORT, HOST);
